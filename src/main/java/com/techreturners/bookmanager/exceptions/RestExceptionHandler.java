3b23e991bb2fa07654941cd112dd137a2cad3479
Adrian Yates <adeyatz@gmail.com>
2023-09-30 13:12:23 +0100

    Refactor: RestExceptionHandler, use handleExceptionInternal mechanism

--- src/main/java/com/techreturners/bookmanager/exceptions/RestExceptionHandler.java
+++ src/main/java/com/techreturners/bookmanager/exceptions/RestExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.techreturners.bookmanager.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.NoSuchElementException;
@@ -12,20 +13,33 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler  {
     @ExceptionHandler({BookNotFoundException.class})
-    public ResponseEntity<Object> handleBookNotFoundException (BookNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    protected ResponseEntity<Object> handleBookNotFoundException (BookNotFoundException exception, WebRequest request) {
+        return handleExceptionInternal(exception,
+                request.getDescription(false) + " : " +  exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Object> handleNoSuchElementException (NoSuchElementException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    protected ResponseEntity<Object> handleNoSuchElementException (NoSuchElementException exception, WebRequest request) {
+        return handleExceptionInternal(exception,
+                request.getDescription(false) + " : " +  exception.getMessage(),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
+
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+    protected ResponseEntity<Object> handleRuntimeException(RuntimeException exception, WebRequest request) {
+        return handleExceptionInternal(exception, exception.getMessage(),
+                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+
+//        return ResponseEntity
+//                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body(exception.getMessage());
     }
 
 
